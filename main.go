0ae22eda861f1b7ffa8b9ecad39603cbf464cd48
Mikhail Petrov <lanak-mu@mail.ru>
2019-09-11 13:03:57 +0300

    1

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"net/http" // пакет для поддержки HTTP протокола
+
+	"log" // пакет для логирования
+)
+
+func HomeRouterHandler(w http.ResponseWriter, r *http.Request) {
+	//	r.ParseForm() //анализ аргументов,
+	http.ServeFile(w, r, "index.html")
+
+	/*fmt.Println(r.Form) // ввод информации о форме на стороне сервера
+
+	fmt.Println("path", r.URL.Path)
+
+	fmt.Println("scheme", r.URL.Scheme)
+
+	fmt.Println(r.Form["url_long"])
+
+	for k, v := range r.Form {
+
+		fmt.Println("key:", k)
+
+		fmt.Println("val:", strings.Join(v, ""))
+
+	}
+
+	fmt.Fprintf(w, "Test!")*/ // отправляем данные на клиентскую сторону
+
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("username")
+	age := r.FormValue("userage")
+	fmt.Fprintf(w, "Имя: %s Возраст: %s", name, age)
+}
+
+func handler2(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	age := r.URL.Query().Get("age")
+	fmt.Fprintf(w, "Имя: %s Возраст: %s", name, age)
+}
+
+func main() {
+
+	http.HandleFunc("/", HomeRouterHandler)
+	http.HandleFunc("/post", handler)
+	http.HandleFunc("/get", handler2)
+
+	err := http.ListenAndServe(":8081", nil) // задаем слушать порт
+
+	if err != nil {
+
+		log.Fatal("ListenAndServe: ", err)
+
+	}
+
+}
---
+++ script.js
@@ -0,0 +1,5 @@
+
+let a = document.querySelector('a');
+a.onclick = () => {
+
+}
